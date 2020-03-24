04538c1f59c6618890c56c84393cd50a28f622ac
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-03-23 22:17:42 -0300

    Adjusting currency format

--- public/scripts.js
+++ public/scripts.js
@@ -1,12 +1,16 @@
 const input = document.querySelector("input[name='price']")
 input.addEventListener("keydown", function(e) {
-    let { value } = e.target
-
-    value = value.replace(/\D/g, "")
-
-    e.target.value = value
-
+    
     setTimeout(function() {
+        let { value } = e.target
+
+        value = value.replace(/\D/g, "")
+
+        value = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value/100)
+
         e.target.value = value
     }, 1)
 })
