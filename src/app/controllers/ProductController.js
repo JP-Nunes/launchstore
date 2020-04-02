0eaa56b637714ced47bd0be5df827e1a09df0a9c
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-04-01 23:43:42 -0300

    Logic to delete images that were rendered through the backend

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -17,7 +17,7 @@ module.exports = {
         const keys = Object.keys(req.body)
 
         for(key of keys) {
-            if(req.body[keys] == "") {return res.send("Please, fill all fields!")}
+            if(req.body[key] == "") {return res.send("Please, fill all fields!")}
         }
 
         if (req.files.length == 0) {return res.send('Please, send at least one image')}
@@ -56,9 +56,18 @@ module.exports = {
         const keys = Object.keys(req.body)
 
         for(key of keys) {
-            if(req.body[keys] == "") {
+            if(req.body[key] == "" && key != "removed_files") {
                 return res.send("Please, fill all fields!")
             }
+        }
+
+        if(req.body.removed_files) {
+            const removedFiles = req.body.removed_files.split(",")
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1)
+
+            const removedFilesPromise = removedFiles.map(id => File.delete(id))
+            await Promise.all(removedFilesPromise)
         }
 
         req.body.price = req.body.price.replace(/\D/g, "")
