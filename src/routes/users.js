f86f068c3cf57edb0b1c8c58cd9893a89d7d8483
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-04-18 18:47:48 -0300

    Creating routes to attend the users related controllers

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,23 +5,23 @@ const SessionController = require('../app/controllers/SessionController')
 const UserController = require('../app/controllers/UserController')
 
 //login/logout
-routes.get('/login', SessionController.loginForm)
-routes.post('/login', SessionController.login)
-routes.post('/logout', SessionController.logout)
+//routes.get('/login', SessionController.loginForm)
+//routes.post('/login', SessionController.login)
+//routes.post('/logout', SessionController.logout)
 
 //reset password / forgot
-routes.get('/forgot-password', SessionController.forgotForm)
-routes.get('/password-reset', SessionController.resetForm)
-routes.post('/forgot-password', SessionController.forgot)
-routes.post('/password-reset', SessionController.reset)
+//routes.get('/forgot-password', SessionController.forgotForm)
+//routes.get('/password-reset', SessionController.resetForm)
+//routes.post('/forgot-password', SessionController.forgot)
+//routes.post('/password-reset', SessionController.reset)
 
 //user register
 routes.get('/register', UserController.registerForm)
-routes.post('/register', UserController.post)
-routes.post('/register', UserController.post)
+//routes.post('/register', UserController.post)
+//routes.post('/register', UserController.post)
 
-routes.get('/', UserController.show)
-routes.put('/:id/update', UserController.update)
-routes.delete('/:id/delete', UserController.delete)
+//routes.get('/', UserController.show)
+//routes.put('/:id/update', UserController.update)
+//routes.delete('/:id/delete', UserController.delete)
 
 module.exports = routes;
