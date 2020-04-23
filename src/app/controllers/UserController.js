5a2fb6a2ba65505b5321f6893029a5de2a764803
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-04-23 11:58:58 -0300

    Validating user register

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,15 +4,24 @@ module.exports = {
    registerForm(req, res) {
       return res.render("user/register")
    },
-   post(req, res) {
-      if(req.body[key] == "") {
-         return res.send('Please, fill all fields!')
+   async post(req, res) {
+      const keys = Object.keys(req.body)
+      
+      for(key of keys) {
+         if(req.body[key] == "") {return res.send('Please, fill all fields!')}
       }
 
-      const { email, cpf_cnpj } = req.body
+      let { email, cpf_cnpj, password, passwordRepeat } = req.body
+      
+      cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
+
       const user = await User.findOne({
          where: {email},
          or: {cpf_cnpj}
       })
+
+      if(user) return res.send('User exists')
+
+      if(password !== passwordRepeat) return res.send('Password mismatch')
    }
 }
