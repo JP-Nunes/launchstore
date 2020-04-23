b009b4e0be1efcebf5e575c74b1f8178489edd76
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-04-23 14:45:45 -0300

    Separating validation logic from controllers

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,23 +5,8 @@ module.exports = {
       return res.render("user/register")
    },
    async post(req, res) {
-      const keys = Object.keys(req.body)
       
-      for(key of keys) {
-         if(req.body[key] == "") {return res.send('Please, fill all fields!')}
-      }
-
-      let { email, cpf_cnpj, password, passwordRepeat } = req.body
-      
-      cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
-
-      const user = await User.findOne({
-         where: {email},
-         or: {cpf_cnpj}
-      })
-
-      if(user) return res.send('User exists')
-
-      if(password !== passwordRepeat) return res.send('Password mismatch')
+   
+      return res.send('passed')
    }
 }
