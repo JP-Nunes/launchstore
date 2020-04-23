9c0a2eed60fbd74fc96508d5f0ac37f929338446
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-04-22 23:27:51 -0300

    Implementing a visual frontend response in case there is an error on cep or cpf_cnpj validation

--- public/scripts.js
+++ public/scripts.js
@@ -192,7 +192,6 @@ const Validate = {
 
       if(results.error) Validate.displayError(input, results.error)
 
-      input.focus()
    },
    displayError(input, error) {
       const div = document.createElement('div')
@@ -211,6 +210,37 @@ const Validate = {
       const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
       if(!value.match(mailFormat)) error = "Email inválido"
+
+      return {
+         error,
+         value
+      }
+   },
+   isCpfCnpj(value) {
+      let error = null
+   
+      const cleanValues = value.replace(/\D/g, "")
+
+      if(cleanValues.length > 11 && cleanValues.length !== 14) {
+         error = "CNPJ incorreto"
+      }
+      else if (cleanValues.length < 12 && cleanValues.length !== 11) {
+         error = "CPF incorreto"
+      }
+
+      return {
+         error,
+         value
+      }
+   },
+   isCep(value) {
+      let error = null
+   
+      const cleanValues = value.replace(/\D/g, "")
+
+      if(cleanValues.length !== 8) {
+         error = "CEP incorreto"
+      }
 
       return {
          error,
