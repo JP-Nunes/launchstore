9174ddee727a8e7e5828bf8649f6b761d8994b01
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-04-23 10:34:03 -0300

    Using post methos to register a new User. Using dynamic query to search if the user is already registered

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,18 @@
+const User = require('../models/User')
+
 module.exports = {
    registerForm(req, res) {
       return res.render("user/register")
+   },
+   post(req, res) {
+      if(req.body[key] == "") {
+         return res.send('Please, fill all fields!')
+      }
+
+      const { email, cpf_cnpj } = req.body
+      const user = await User.findOne({
+         where: {email},
+         or: {cpf_cnpj}
+      })
    }
 }
