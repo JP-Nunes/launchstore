5c82eba8fb44b00e4c4d1deed917dcedbb84a4e1
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-05-09 20:49:56 -0300

    Adjusting controllers to the new base model

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -1,65 +1,63 @@
-const { formatPrice } = require('../../lib/utils')
-
 const Product = require('../models/Product')
 
+const { formatPrice } = require('../../lib/utils')
+
 module.exports = {
-    async index(req, res) {
+   async index(req, res) {
+      try {
+         let results
+         let params = {}
 
-        try {
-            let results,
-                params = {}
+         const { filter, category } = req.query
 
-            const { filter, category } = req.query
+         if(!filter) return res.redirect("/")
 
-            if(!filter) return res.redirect("/")
+         params.filter = filter
 
-            params.filter = filter
+         if(category) {
+            params.category = category
+         }
 
-            if(category) {
-                params.category = category
-            }
+         let products = await Product.search(params)
 
-            results = await Product.search(params)
+         async function getImage(productId) {
+            let files = await Product.files(productId)
+            files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path
+            .replace("public", "").replace(/\\/g, "/")}`)
 
-            async function getImage(productId) {
-                let results = await Product.files(productId)
-                const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path
-                    .replace("public", "").replace(/\\/g, "/")}`)
+            return files[0]
+         }
 
-                return files[0]
-            }
+         const productsPromise = products.map(async product => {
+            product.img = await getImage(product.id)
+            product.oldPrice = formatPrice(product.old_price)
+            product.price = formatPrice(product.price)
+            return product
+         })
 
-            const productsPromise = results.rows.map(async product => {
-                product.img = await getImage(product.id)
-                product.oldPrice = formatPrice(product.old_price)
-                product.price = formatPrice(product.price)
-                return product
-            })
-
-            const products = await Promise.all(productsPromise)
-
+         products = await Promise.all(productsPromise)
             
-            const search = {
-                term: req.query.filter,
-                total: products.length
-            }
+         const search = {
+            term: req.query.filter,
+            total: products.length
+         }
             
-            const categories = products.map(product => ({
-                id: product.category_id,
-                name: product.category_name
-            })).reduce((filteredCategories, category) => {
+         const categories = products.map(product => ({
+            id: product.category_id,
+            name: product.category_name
+         })).reduce((filteredCategories, category) => {
 
-                const found = filteredCategories.some(categ => categ.id == category.id)
+         const found = filteredCategories.some(categ => categ.id == category.id)
 
-                if(!found) filteredCategories.push(category)
+         if(!found) filteredCategories.push(category)
                 
-                return filteredCategories
-            }, []) 
+         return filteredCategories
+         }, []) 
 
-            return res.render("search/index", { products, search, categories })
-        }
-        catch(err) {
-            console.error(err)
-        }
+         return res.render("search/index", { products, search, categories })
+      
+      } catch(err) {
+         console.error(err)
+      }
     }
 }
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,4 +1,9 @@
 const User = require('../models/User')
+const Product = require('../models/Product')
+
+const { hash } = require('bcryptjs')
+const { unlinkSync } = require('fs')
+
 const { formatCep, formatCpfCnpj } = require('../../lib/utils')
 
 module.exports = {
@@ -6,20 +11,37 @@ module.exports = {
       return res.render("user/register")
    },
    async show(req, res) {
-      const { user } = req
+      try {
+         const { user } = req
 
-      user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj)
-      user.cpf_cnpj = formatCep(user.cep)
+         user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj)
+         user.cpf_cnpj = formatCep(user.cep)
 
-      return res.render('user/index', { user })
+         return res.render('user/index', { user })
+      } catch (err) {
+         console.error(err)
+      }
    },
    async post(req, res) {
       
-      const userId = await User.create(req.body)
+      try {
+         let { name, email, password, cpf_cnpj, cep, address } = req.body
 
-      req.session.userId = userId
+         password = await hash(password, 8)
+         data.cpf_cnpj.replace(/\D/g, "") 
+         data.cep.replace(/\D/g, "")
+         
+         const userId = await User.create({
+            name, email, password,
+            cpf_cnpj, cep, address
+         })
 
-      return res.redirect('/users')
+         req.session.userId = userId
+
+         return res.redirect('/users')
+      } catch (err) {
+         console.error(err)
+      }
    },
    async update(req, res) {
       try {
@@ -51,8 +73,26 @@ module.exports = {
    },
    async delete(req, res) {
       try {
+         const products = await Product.findAll({where: { user_id: req.body.id }})
+         
+
+
+         const allFilesPromise = products.map(product => Product.files(product.id))
+
+         let promiseResults = await Promise.all(allFilesPromise)
+
          await User.delete(req.body.id)
          req.session.destroy()
+                    
+         promiseResults.map(results => {
+            results.rows.map(file => { 
+               try {
+                  unlinkSync(file.path)  
+               } catch (err) {
+                  console.error(err)
+               }
+            })
+         })
 
          return res.render('session/login', {
             success: 'Conta deletada com sucesso!'
