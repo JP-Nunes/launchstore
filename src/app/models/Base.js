45cc92b9c48c2673ed471a035b06faee49354190
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-05-10 20:00:49 -0300

    Fixing typos

--- src/app/models/Base.js
+++ src/app/models/Base.js
@@ -13,6 +13,7 @@ function find(filters, table) {
       })
    }
 
+
    return db.query(query)
 }
 
@@ -44,12 +45,12 @@ const Base = {
 
          Object.keys(fields).map(key => {
             keys.push(key)
-            values.push(fields[key])
+            values.push(`'${fields[key]}'`)
          })
 
          const query = `
             INSERT INTO ${this.table} (${keys.join(',')})
-            VALUES (${values})
+            VALUES (${values.join(',')})
             RETURNING id
          `
 
