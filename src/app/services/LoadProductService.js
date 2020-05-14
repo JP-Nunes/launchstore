40422be89989ba11061b34e9636b2b9af430d1e9
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-05-13 22:58:36 -0300

    Fixing the missing 'async'

--- src/app/services/LoadProductService.js
+++ src/app/services/LoadProductService.js
@@ -35,7 +35,7 @@ const LoadService = {
 
       return this[service]()
    },
-   product() {
+   async product() {
       try {
          const product = await Product.findOne(this.filter)
          
@@ -44,7 +44,7 @@ const LoadService = {
          console.error(err)
       }
    },
-   products() {
+   async products() {
       try {
          const products = await Product.findAll(this.filter)
          const productsPromise = products.map(format)
