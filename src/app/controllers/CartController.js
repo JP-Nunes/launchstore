74907b2e74f811df442b602d80cdc7fc885e0d4a
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-05-22 23:43:27 -0300

    Implementing cart's add-one

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -6,16 +6,25 @@ const LoadProductService = require('../services/LoadProductService')
 module.exports = {
    async index(req, res) {
       try {
-         const product = LoadProductService.load('product', { where: { id: 1 } })
-
          let { cart } = req.session
 
-         cart = Cart.init(cart).addOne(product)
+         cart = Cart.init(cart)
 
          return res.render('cart/index', { cart })
       }
       catch(err) {
          console.error(err)
       }
+   },
+   async addOne(req, res) {
+      const { id } = req.params
+
+      const product = LoadProductService.load('product', { where: { id } })
+
+      let { cart } = req.session
+
+      req.session.cart = Cart.init(cart).addOne(product)
+
+      return res.redirect('/cart')
    }
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,5 +4,6 @@ const routes = express.Router()
 const CartController = require('../app/controllers/CartController')
 
 routes.get('/', CartController.loginForm)
+routes.post('/:id/add-one', CartController.addOne)
 
 module.exports = routes;
