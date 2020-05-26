8575d6e68097fe8a4051d99063916c4fbfefd851
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-05-25 23:09:55 -0300

    remove-one route

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -3,7 +3,8 @@ const routes = express.Router()
 
 const CartController = require('../app/controllers/CartController')
 
-routes.get('/', CartController.loginForm)
+routes.get('/', CartController.index)
 routes.post('/:id/add-one', CartController.addOne)
+routes.post('/:id/remove-one', CartController.removeOne)
 
 module.exports = routes;
