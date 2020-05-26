7fbdabb2b736794e627d6912f8513ba6b9d3a5c0
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-05-25 23:10:16 -0300

    Fixing typo

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -1,10 +1,10 @@
 const { formatPrice } = require('./utils')
 
 const Cart = {
-   init(oldCar) {
-      if(oldCar) {
-         this.items = oldCar.items
-         this.total  = oldCar.total
+   init(oldCart) {
+      if(oldCart) {
+         this.items = oldCart.items
+         this.total  = oldCart.total
       } else {
          this.items = []
          this.total = {
@@ -42,7 +42,7 @@ const Cart = {
    
       this.total.quantity++
       this.total.price += inCart.product.price
-      this.formatedPrice = formatPrice(inCart.product.price)
+      this.formatedPrice = formatPrice(this.total.price)
    
       return this
    },
@@ -84,32 +84,8 @@ const Cart = {
       return this
    },
    getCartItem(productId) {
-      return this.items.find(item => item.product.id == product.id)
+      return this.items.find(item => item.product.id == productId)
    }
 }
-
-const product = {
-   id: 1,
-   price: 199,
-   quantity: 2
-}
-
-const product2 = {
-   id: 2,
-   price: 299,
-   quantity: 1
-}
-
-console.log('add first cart item')
-let oldCart = Cart.init().addOne(product)
-console.log(oldCart)
-
-console.log('add second cart item')
-oldCart = Cart.init(oldCart).addOne(product)
-console.log(oldCart)
-
-console.log('add third cart item')
-oldCart = Cart.init(oldCart).delete(product.id)
-console.log(oldCart)
 
 module.exports = Cart
